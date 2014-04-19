9359020487269414e7e28b6cf92e9b61ef8ff8a0
Saulo Harã <saulohara@gmail.com>
2014-04-19 19:10:59 -0300

    Criando projeto

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+TrupeMoney::Application.config.secret_key_base = '8eef30c93fbd2496226e25a315b9d7cfec9f257d4e00e0cc508c9db0d432d9208f050276af2c899be8c75de8687798f9effacdbad65d1851f9afef50a184b509'
