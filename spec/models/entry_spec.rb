e1c25d4830bd316d83c55448499157199006a086
Marcio <mmiranda1984@globo.com>
2014-05-13 14:23:59 -0300

    Criando arquivos spec para entry e event

---
+++ spec/models/entry_spec.rb
@@ -0,0 +1,37 @@
+require "spec_helper"
+
+describe Entry do
+	
+	#before(:each) do
+    #	@user = FactoryGirl.build(:user, :marcio)
+    #end
+
+    it "validates presence of user" do
+
+    end
+
+    it "validates presence of amount" do
+
+    end
+
+    it "validates presence of reference date" do
+
+    end
+
+    it "validates presence of master user" do
+
+    end
+
+    it "validates presence of amount type" do
+
+    end
+
+    it "validates amount greater than 0" do
+
+    end
+
+    it "validates values of amout type" do
+
+    end
+
+  end
---
+++ spec/service/event_service_spec.rb
@@ -0,0 +1,44 @@
+require "spec_helper"
+
+describe Event_Service do
+	
+	#before(:each) do
+    #	@user = FactoryGirl.build(:user, :marcio)
+    #end
+
+    it "validates presence of name" do
+
+    end
+
+    it "validates presence of amount" do
+
+    end
+
+    it "validates presence of reference date" do
+
+    end
+
+    it "validates presence of participants" do
+
+    end
+
+    it "validates amount greater than 0" do
+
+    end
+
+    it "validates presence of amount_participant for each participant" do
+
+    end
+
+    it "validates amount_participant greater than 0 for each participant" do
+
+    end
+
+    it "validates if all participants in the event" do
+
+    end
+
+    it "validates if amount of event equals sum amount_participants" do 
+
+    end
+end
